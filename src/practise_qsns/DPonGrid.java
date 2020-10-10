1875240ffcc8c3ef5289b49dc6cbd889d247c675
RishabhStark <rishavh408@gmail.com>
2020-10-10 23:01:49 +0530

    Dp on grid

--- src/practise_qsns/DPonGrid.java
+++ src/practise_qsns/DPonGrid.java
@@ -1,6 +1,14 @@
 package practise_qsns;
 
 class DpOnGrid {
+
+//    A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+//
+//    The robot can only move either down or right at any point in time. The robot is trying to
+//    reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+//
+//    How many possible unique paths are there?
+
     public int uniquePaths(int m, int n) {
         int arr[][]=new int[m][n];
         for(int i=0;i<m;i++) {
